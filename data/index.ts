2586b8b66e339bc8bfd8c4d3405dea67e77d1b69
Mahdi <mahdirezaii490@gmail.com>
2024-09-02 05:13:11 +0330

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -43,7 +43,7 @@ export const navItems = [
      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
      imgClassName: "",
      titleClassName: "justify-start",
-     img: "/grid.svg",
+     img: "./grid.svg",
      spareImg: "./b4.svg",
    },
  
@@ -54,7 +54,7 @@ export const navItems = [
      className: "md:col-span-3 md:row-span-2",
      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
      titleClassName: "justify-center md:justify-start lg:justify-center",
-     img: "/b5.svg",
+     img: "./b5.svg",
      spareImg: "./grid.svg",
    },
    {
@@ -74,32 +74,32 @@ export const navItems = [
      id: 1,
      title: "3D Solar System Planets to Explore",
      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-     img: "/p1.svg",
-     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+     img: "./p1.svg",
+     iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./fm.svg"],
      link: "/ui.earth.com",
    },
    {
      id: 2,
      title: "Yoom - Video Conferencing App",
      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-     img: "/p2.svg",
-     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+     img: "./p2.svg",
+     iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./stream.svg", "./c.svg"],
      link: "/ui.yoom.com",
    },
    {
      id: 3,
      title: "AI Image SaaS - Canva Application",
      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-     img: "/p3.svg",
-     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+     img: "./p3.svg",
+     iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
      link: "/ui.aiimg.com",
    },
    {
      id: 4,
      title: "Animated Apple Iphone 3D Website",
      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-     img: "/p4.svg",
-     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+     img: "./p4.svg",
+     iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
      link: "/ui.apple.com",
    },
  ];
@@ -166,7 +166,7 @@ export const navItems = [
      id: 5,
      name: "docker.",
      img: "/dock.svg",
-     nameImg: "/dockerName.svg",
+     nameImg: "./dockerName.svg",
    },
  ];
  
@@ -176,7 +176,7 @@ export const navItems = [
      title: "Frontend Engineer Intern",
      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
      className: "md:col-span-2",
-     thumbnail: "/exp1.svg",
+     thumbnail: "./exp1.svg",
    },
    {
      id: 2,
@@ -190,14 +190,14 @@ export const navItems = [
      title: "Freelance App Dev Project",
      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
      className: "md:col-span-2", // change to md:col-span-2
-     thumbnail: "/exp3.svg",
+     thumbnail: "./exp3.svg",
    },
    {
      id: 4,
      title: "Lead Frontend Developer",
      desc: "Developed and maintained user-facing features using modern frontend technologies.",
      className: "md:col-span-2",
-     thumbnail: "/exp4.svg",
+     thumbnail: "./exp4.svg",
    },
  ];
  
